bff738b41943e111edf021c51979f3db494c6d6e
Rhys⁣⁣⁣⁣⁣⁣⁣⁣ <98863820+rhysdh540@users.noreply.github.com>
2022-11-13 13:34:16 -0500

    Delete Main.java
    it's just for testing, nobody really needs it

--- projects/Main.java
+++
@@ -1,9 +0,0 @@
-import java.io.PrintStream;
-import java.util.Arrays;
-
-public class Main {
-    static PrintStream so = System.out;
-    public static void main(String[] args) {
-        
-    }
-}
