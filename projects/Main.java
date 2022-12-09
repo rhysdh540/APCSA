0fa72efc695a0886cdb7e52833b7fc7615115e5c
rhysdh540 <rhys_dehaan@ryecountryday.org>
2022-12-09 11:46:04 -0500

    hi

--- projects/Main.java
+++ projects/Main.java
@@ -8,6 +8,7 @@ public class Main {
     static PrintStream so = System.out;
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] amogus) {
-        Emails.sendEmail("davis_clarke@ryecountryday.org", "rhys_dehaan@ryecountryday.org", SECRETDONOTCOMMITTOGITHUB.SCHOOL_PASSWORD, "hi davis", "hi davis");
+        Emails.sendEmail("davis_clarke@ryecountryday.org", "rhys_dehaan@ryecountryday.org", SECRETDONOTCOMMITTOGITHUB.SCHOOL_PASSWORD, "hi davis", "i promise this is the last one");
+        so.println("sent");
     }
 }
