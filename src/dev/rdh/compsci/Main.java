9e218bede3567bed526ef5ae19a264353c771e9d
rhysdh540 <rhys_dehaan@ryecountryday.org>
2023-06-14 15:14:28 -0400

    main

--- src/dev/rdh/compsci/Main.java
+++ src/dev/rdh/compsci/Main.java
@@ -2,17 +2,13 @@ package dev.rdh.compsci;
 
 import dev.rdh.compsci.at1.ch0.MonteCarlo;
 
-import java.util.*;
-import java.io.PrintStream;
 
 /**
  * testing class
  */
 @SuppressWarnings({"unused", "NonAsciiCharacters"})
 public class Main {
-    public static PrintStream so = System.out;
-    public static Scanner sc = new Scanner(System.in);
     public static void main(String...ඞ) {
-        MonteCarlo.run();
+        MonteCarlo.run(false);
     }
 }
